98ff33571b377f8f0020b31ee60ceeb4c063de63
Bear <648070256@qq.com>
2016-05-07 20:08:36 +0800

    first commit

---
+++ MusicWidgetAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicWidgetAnimation
+//
+//  Created by Bear on 16/5/7.
+//  Copyright © 2016年 Bear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
